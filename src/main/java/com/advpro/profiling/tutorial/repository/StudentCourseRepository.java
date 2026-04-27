6af8715e19523c610651b2273566e3823cb17dd8
Lessyarta Kamali Sopamena Pirade <lessyarta.kamali@ui.ac.id>
2026-04-27 20:22:29 +0700

    [Refactoring] - Optimizing method getAllStudentWithCourse

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
@@ -1,7 +1,7 @@
 package com.advpro.profiling.tutorial.repository;
 
-import com.advpro.profiling.tutorial.model.Student;
 import com.advpro.profiling.tutorial.model.StudentCourse;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +12,9 @@ import java.util.List;
  */
 @Repository
 public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
+
     List<StudentCourse> findByStudentId(Long studentId);
+
+    @EntityGraph(attributePaths = {"student", "course"})
+    List<StudentCourse> findAll();
 }
